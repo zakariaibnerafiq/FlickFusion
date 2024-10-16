2f76dc9fa7b2a62e79ddafcedba7abaf88e32775
NotSoLucky <88020849+not-solucky@users.noreply.github.com>
2024-10-16 09:02:34 +0600

    Project intiated

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "FlickFusion",
+  description: "A Movie Streaming Service",
+};
+
+export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
