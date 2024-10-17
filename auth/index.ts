84e39f2fc1666530d40a45ec7289cc11b34b008b
Ahanaf Tanvir <ahanaf.tanvir@g.bracu.ac.bd>
2024-10-17 14:19:38 +0600

    implemented Prisma

--- auth/index.ts
+++ auth/index.ts
@@ -1,3 +1,4 @@
+import db from "@lib/db";
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 
@@ -24,11 +25,49 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
         async signIn({ user, profile }) {
             console.log('USER: ', user);
             console.log('PROFILE: ', profile);
+
+
             //check if user is in database
             //if not create user in database
             //if user is in database, return true
             //if user is not in database, return false
-            return true
+
+            try {
+                if (!profile?.email) {
+                    return false;
+                }
+
+                const existingUser = await db.user.findFirst({
+                    where: {
+                        email: profile.email
+                    }
+                })
+
+                if (existingUser) {
+                    return true
+                } else {
+                    const newUser = await db.user.create({
+                        data: {
+                            username: profile!.name!,
+                            email: profile!.email,
+                            password: 'empty',
+                            profilePic: profile!.picture,
+                        }
+                    })
+                    if (newUser) {
+                        return true
+                    } else {
+                        throw new Error('User not created')
+                    }
+
+                }
+            } catch (error) {
+                console.error('Error in signIn callback:', error)
+                return false
+            } finally {
+                await db.$disconnect()
+            }
+
         },
         async jwt({ token, user, profile }) {
             if (user) {
