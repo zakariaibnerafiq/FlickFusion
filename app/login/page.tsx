bcf8e7ab2b8a9d4276b36db3347b7fa8bf5ed3db
NotSoLucky <88020849+not-solucky@users.noreply.github.com>
2024-10-17 22:15:05 +0600

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -32,7 +32,7 @@ const Login = () => {
                   <div className="p-6 grid gap-4 pt-0">
                     <button onClick={handleLogin} className='inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium h-9 px-4 py-2 [&_svg]:size-4 [&_svg]:shrink-0 text-base-800 bg-gradient-to-r from-peach to-red'>
                       <svg role="img" viewBox="0 0 24 24" className="mr-2 h-4 w-4"><path fill="currentColor" d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"></path></svg>
-                      Google
+                      Continue with Google
                     </button>
                     <div className="relative">
                       <div className="absolute inset-0 flex items-center">
