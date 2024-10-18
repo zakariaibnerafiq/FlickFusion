8dffc1658b1a209683c3235a04eaf2342714e810
NotSoLucky <88020849+not-solucky@users.noreply.github.com>
2024-10-18 12:36:33 +0600

    main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@components/Navbar";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider} from "next-auth/react";
 // change color theme
 
 
