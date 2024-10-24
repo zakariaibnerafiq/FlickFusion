e35861555f92bbcf156855c3cf7cc4bd013e38da
NotSoLucky <88020849+zakariaibnerafiq@users.noreply.github.com>
2024-10-24 16:35:33 +0600

    admin can add movie using form

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -11,11 +11,11 @@ const Sidebar = () => {
     return (
         <div className="z-20 hidden w-64 overflow-y-auto md:block flex-shrink-0 ">
             <div className="flex flex-col h-full">
-                <div className="flex flex-col flex-1 gap-1">
-                    <ActiveLink name={'Dashboard'} href={'/admin'} activeClassName={'text-mauve bg-base-700'} className = {'flex items-center justify-between px-4 py-2 text-text hover:bg-base-100 hover:text-peach rounded-md font-semibold'} />
-                    <ActiveLink name={'Add Movie'} href={'/admin/addmovie'} activeClassName={'text-mauve bg-base-700'} className = {'flex items-center justify-between px-4 py-2 text-text hover:bg-base-100 hover:text-peach rounded-md font-semibold'} />
-                    <ActiveLink name={'All Movies'} href={'/admin/movies'} activeClassName={'text-mauve bg-base-700'} className = {'flex items-center justify-between px-4 py-2 text-text hover:bg-base-100 hover:text-peach rounded-md font-semibold'} />
-                    <ActiveLink name={'All Users'} href={'/Users'} activeClassName={'text-mauve bg-base-700'} className = {'flex items-center justify-between px-4 py-2 text-text hover:bg-base-100 hover:text-peach rounded-md font-semibold'} />
+                <div className="flex flex-col flex-1 gap-1 text-text">
+                    <ActiveLink name={'Dashboard'} href={'/admin'} activeClassName={'text-mauve bg-base-700'} className = {'flex items-center justify-between px-4 py-2 hover:bg-base-100 hover:text-peach rounded-md font-semibold'} />
+                    <ActiveLink name={'Add Movie'} href={'/admin/addmovie'} activeClassName={'text-mauve bg-base-700'} className = {'flex items-center justify-between px-4 py-2 hover:bg-base-100 hover:text-peach rounded-md font-semibold'} />
+                    <ActiveLink name={'All Movies'} href={'/admin/movies'} activeClassName={'text-mauve bg-base-700'} className = {'flex items-center justify-between px-4 py-2 hover:bg-base-100 hover:text-peach rounded-md font-semibold'} />
+                    <ActiveLink name={'All Users'} href={'/Users'} activeClassName={'text-mauve bg-base-700'} className = {'flex items-center justify-between px-4 py-2 hover:bg-base-100 hover:text-peach rounded-md font-semibold'} />
                 </div>
             </div>
 
